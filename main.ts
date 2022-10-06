c0b4cc8b1315014bab2e4c4dd86d0cce3e0609c7
xsubo5 <78550459+xsubo5@users.noreply.github.com>
2022-10-06 22:30:15 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+input.onPinPressed(TouchPin.P1, function () {
+    basic.pause(500)
+    servos.P0.setAngle(0)
+    basic.pause(500)
+    servos.P0.setAngle(120)
+    counter += 1
+    if (counter == 1) {
+        basic.showLeds(`
+            # . # . #
+            # # # # #
+            . . . . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (counter == 2) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (counter == 3) {
+        basic.showLeds(`
+            # . . . #
+            . # # # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (counter == 4) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        counter = 0
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . . . .
+            # # . # #
+            . . . . .
+            `)
+    }
+})
+let counter = 0
+counter = 0
+servos.P0.setAngle(120)
+basic.showIcon(IconNames.Asleep)
